d5f5c219ea240b2906b65c57151a46cefd824313
Matt Rothenberg <mattrothenberg@gmail.com>
2017-05-05 08:46:56 -0400

    Ignore selectors during uncss step

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ gulp.task('uncss', function() {
     .pipe(uncss({
       html: [
         'dist/**/*.html'
-      ]
+      ],
+      ignore: [/code/g, /pre/g, /^.token/]
     }))
     .pipe(gulp.dest('dist/assets', {overwrite: true}));
 });
